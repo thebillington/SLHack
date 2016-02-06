3d53e860c69b810beb4a30b658415f1ad0de8b47
Billy Rebecchi <b.rebecchi@newcastle.ac.uk>
2016-02-06 14:23:07 +0000

    Add volatility purchasing of stocks, ensure every stock turns a profit.

--- src/trading_game/TradingStrategy.java
+++ src/trading_game/TradingStrategy.java
@@ -11,35 +11,77 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TradingStrategy extends BaseTradingStrategy {
-	
+
 	List<List> companies = new ArrayList<List>();
-	
-	public TradingStrategy (){
+	private double lastPurchasePrice;
+	private double lastPurchaseDay;
+
+	public TradingStrategy() {
 		// Initialise any variables needed.
-		
-		
+
 	}
-	
+
 	@Override
 	public DailyOutput makeDailyTrade(DailyInput input) throws InsufficientFundsException, InsufficientSharesException {
-		
-		// Use the trading manager to make trades based on input.	
-		if(input.getDay() == 1){
+
+		// Use the trading manager to make trades based on input.
+		if (input.getDay() == 1) {
 			companies.add(new ArrayList<DayInfo>());
 		}
-		
-		List<DayInfo> companyInfo = companies.get(companies.size()-1);
+
+		List<DayInfo> companyInfo = companies.get(companies.size() - 1);
 		companyInfo.add(new DayInfo(input.getHigh(), input.getLow(), input.getOpen(), input.getClose()));
-		
+
 		DailyOutput output;
-		if (input.getDay() % 2 == 0) {
-			output = tradingManager.buyMaxNumberOfShares(input);
-		} else {
+		
+		if(input.getDay() > 8) {
+			
+			List<DayInfo> lastWeek = companyInfo.subList(input.getDay() - 8, input.getDay() - 1);
+			
+			double vol = weekVolatility(lastWeek);
+			
+			if(vol > 0.05) {
+				if(weekIncrease(lastWeek) > 0.01) {
+					output = tradingManager.buyMaxNumberOfShares(input);
+					lastPurchasePrice = input.getClose();
+					lastPurchaseDay = input.getDay();
+				}
+				else {
+					if(input.getDay() - lastPurchaseDay > 7 && input.getClose() > lastPurchasePrice) {
+						output = tradingManager.sellAllShares(input);
+					}
+					else {
+						output = tradingManager.doNothing(input);
+					}
+				}
+			}
+			else {
+				output = tradingManager.doNothing(input);
+			}
+			
+		}
+		else {
+			output = tradingManager.doNothing(input);
+		}
+		if(input.getDay() - lastPurchaseDay > 7 && input.getClose() > lastPurchasePrice) {
 			output = tradingManager.sellAllShares(input);
 		}
 		
 		return output;
 	}
 	
+	private double weekVolatility(List<DayInfo> lastWeek) {
+		double volatility = 0;
+		for(DayInfo d : lastWeek) {
+			volatility += d.getVolatitility();
+		}
+		return volatility / lastWeek.size();
+	}
 	
+	private double weekIncrease(List<DayInfo> lastWeek) {
+		double weekOpen = lastWeek.get(0).getOpen();
+		double weekClose = lastWeek.get(lastWeek.size()-1).getClose();
+		return weekClose - weekOpen;
+	}
+
 }
